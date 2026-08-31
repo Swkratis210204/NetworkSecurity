cd929fcda64cdc73ad45902036be1bc838e5893c
swkra <swkratisgiannoutsos@gmail.com>
2026-08-31 18:04:44 +0300

    Data validation is done

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -5,6 +5,14 @@ from dataclasses import dataclass
 class DataIngestionArtifact:
     # Path where the training dataset will be stored
     training_file_path: str
-
     # Path where the testing dataset will be stored
     test_file_path: str
+    
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    valid_train_file_path:str
+    valid_test_file_path:str
+    invalid_train_file_path:str
+    invalid_test_file_path:str
+    drift_report_file_path:str
