57b02dd673d5fe9b2b771565b8f1ee9b460f5dda
swkra <swkratisgiannoutsos@gmail.com>
2026-09-14 19:15:28 +0300

    connected project to aws s3 bucket

---
+++ networksecurity/cloud/s3_syncer.py
@@ -0,0 +1,8 @@
+import subprocess
+
+class S3Sync:
+    def sync_folder_to_s3(self,folder,aws_bucket_url):
+        subprocess.run(["aws","s3","sync",folder,aws_bucket_url],check=True)
+
+    def sync_folder_from_s3(self,folder,aws_bucket_url):
+        subprocess.run(["aws","s3","sync",aws_bucket_url,folder],check=True)
